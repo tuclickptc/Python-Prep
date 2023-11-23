2f7a5c1324e31d67e57c9448560679dac336b346
Jorge Ortega <tuclickptc2@gmail.com>
2023-11-23 16:53:37 -0400

    actividades modulo 5

--- M05_estructuradedatos/Prep_Course_Homework_05.py
+++ M05_estructuradedatos/Prep_Course_Homework_05.py
@@ -6,7 +6,8 @@
 # 1) Crear una lista que contenga nombres de ciudades del mundo que contenga más de 5 elementos e imprimir por pantalla
 
 # In[3]:
-
+paises = ['Venezuela', 'Bolivía', 'Colombia', 'Ecuador', 'Peru']
+print(paises)
 
 
 
@@ -14,6 +15,7 @@
 
 # In[4]:
 
+print('El segundo elemento de la lista de países es: ', paises[1])
 
 
 
@@ -21,7 +23,7 @@
 
 # In[8]:
 
-
+print('Estos son los elementos del 2 al 4: ', paises[1:4])
 
 
 
@@ -29,7 +31,7 @@
 
 # In[12]:
 
-
+print('El tipo de dato es: ', type(paises))
 
 
 
@@ -37,7 +39,7 @@
 
 # In[14]:
 
-
+print(paises[2:])
 
 
 
@@ -45,7 +47,7 @@
 
 # In[15]:
 
-
+print(paises[:4])
 
     
 
@@ -54,9 +56,10 @@
 
 # In[16]:
 
-
-
-
+paises.append('Uruguay')
+paises.append('Venezuela')
+print(paises)
+# no arroja error, los agrega sin problema
 
 
 
@@ -66,8 +69,8 @@
 
 # In[20]:
 
-
-
+paises.insert(3, 'Paraguay')
+print(paises)
 
 
 # In[21]:
@@ -79,14 +82,16 @@
 
 # In[22]:
 
-
+paises_2 = ['Chile', 'Brasil', 'Argentina']
+paises.extend(paises_2)
+print(paises)
 
 
 # 10) Encontrar el índice de la ciudad que en el punto 7 agregamos duplicada. ¿Se nota alguna particularidad?
 
 # In[23]:
 
-
+print(paises.index('Venezuela'))
 
 
 
@@ -94,7 +99,7 @@
 
 # In[24]:
 
-
+# da un error 
 
 
 
@@ -102,15 +107,16 @@
 
 # In[25]:
 
-
-
+paises.pop()
+print(paises)
 
 
 # 13) ¿Qué pasa si el elemento a eliminar no existe?
 
 # In[27]:
 
-
+paises.pop(7)
+print(paises)
 
 
 
@@ -119,35 +125,42 @@
 # In[28]:
 
 
-
+ultimo_elemento = paises.pop()
+print(ultimo_elemento)
 
 
 # 15) Mostrar la lista multiplicada por 4
 
 # In[29]:
 
-
+print(paises*4)
 
 
 # 16) Crear una tupla que contenga los números enteros del 1 al 20
 
 # In[32]:
 
-
-
+lista = []
+for i in range(20):
+    lista.append(i+1)
+dupla = tuple(lista)
+print(dupla)
 
 # 17) Imprimir desde el índice 10 al 15 de la tupla
 
 # In[35]:
 
-
+print(dupla[11:16])
 
 
 # 18) Evaluar si los números 20 y 30 están dentro de la tupla
 
 # In[41]:
 
-
+if dupla in [20, 30]:
+  print('Los números 20 y 30 están dentro de la dupla.')
+else:
+  print('Los números 20 y 30 no están dentro de la dupla.')
 
 
 
@@ -155,23 +168,27 @@
 
 # In[48]:
 
-
-
-
+elemento = 'Paris'
+if elemento not in paises:
+  paises.append(elemento)
+print(paises)
 
 # 20) Mostrar la cantidad de veces que se encuentra un elemento específico dentro de la tupla y de la lista
 
 # In[51]:
 
+for l in paises:
+  print(l+' está '+str(paises.count(l))+' veces')
 
-
-
+for d in dupla:
+  print(str(d)+' está '+str(paises.count(d))+' veces')
 
 # 21) Convertir la tupla en una lista
 
 # In[52]:
 
-
+lista = list(dupla)
+print(lista)
 
 
 
@@ -179,16 +196,20 @@
 
 # In[55]:
 
-
-
+a, b, c = dupla[:3]
+print(a, b, c)
 
 
 # 23) Crear un diccionario utilizando la lista crada en el punto 1, asignandole la clave "ciudad". Agregar tambien otras claves, como puede ser "Pais" y "Continente".
 
 # In[57]:
 
-
-
+pais = {
+  'pais': paises,
+  'ciudad': ['Mérida', 'Barinas', 'Zulia', 'Falcón'],
+  'continente': 'America'
+}
+print(pais)
 
 
 
@@ -196,13 +217,15 @@
 
 # In[59]:
 
-
+for k in pais.keys():
+  print(k)
 
 
 # 25) Imprimir las ciudades a través de su clave
 
 # In[61]:
 
-
+for k in pais.keys():
+  print(pais[k])
 
 
